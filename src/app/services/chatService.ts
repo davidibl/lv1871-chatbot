6df80289d0455b87f3d54570e91c3b7bd5dd5fb1
David Ibl <david.ibl@xnoname.com>
2017-11-22 22:20:48 +0100

    erste arbeiten am service

--- src/app/services/chatService.ts
+++ src/app/services/chatService.ts
@@ -1,6 +1,26 @@
+import { KundennummerService } from './kundennummerService';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/elementAt';
 
 @Injectable()
 export class ChatService {
 
+    private _answers = [
+        'Uiuiuiuiui',
+        'Sowas aber auch :-O',
+        'Was geht alter?',
+    ];
+
+    public constructor(private _kundennummerservice: KundennummerService) {}
+
+    public answer(kundennummer: number, message: string) {
+
+    }
+
+    private getRandomMessage() {
+        return Observable
+            .of(this._answers)
+            .elementAt(Math.floor((Math.random() * 3)));
+    }
 }
