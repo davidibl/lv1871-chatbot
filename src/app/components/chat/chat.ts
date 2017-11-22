397ba900c08f8f0d3f76e03ac4cb475b6a362c96
David Ibl <david.ibl@xnoname.com>
2017-11-22 21:22:20 +0100

    step1

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -1,7 +1,10 @@
+import { Observable } from 'rxjs/Observable';
 import { DataService } from './../../services/dataService';
 import { KundennummerService } from './../../services/kundennummerService';
 import { Component, OnInit } from '@angular/core';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/zip';
 
 @Component({
     selector: 'lv-chat',
@@ -11,10 +14,26 @@ import { ReplaySubject } from 'rxjs/ReplaySubject';
 export class ChatComponent implements OnInit {
 
     public messageStream = new ReplaySubject<string>();
+    public messages = new Array<string>();
 
     public constructor(private _kundennummerService: KundennummerService,
                        private _dataService: DataService) {}
 
     ngOnInit(): void {
+        this.messageStream
+            .delay(1000)
+            .subscribe(message => this.messages.push(message));
+
+        Observable
+            .zip(
+                this._kundennummerService.getKundennummer(),
+                this._dataService.getData(),
+            )
+            .subscribe(args => {
+                const auskunft = args[1];
+                const kundennummer = args[0];
+                this.messageStream.next(`Hallo Herr ${auskunft.kunde.name},` +
+                    `wie geht es Ihnen? Übrigens, Ihre Kundennummer ist ${kundennummer}`);
+            });
     }
 }
